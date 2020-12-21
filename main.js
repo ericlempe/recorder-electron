1a070617214011d67379259a0983ecc830e38eed
eric <ericlempe1994@gmail.com>
2020-12-21 10:09:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+let win;
+const appUrl = `file://${__dirname}/index.html`;
+
+function createElectronShell(){
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	win.loadURL(appUrl);
+	win.webContents.openDevTools();
+	win.on('closed', () => {
+		win = null;
+	})
+}
+
+app.on('ready', createElectronShell);
+
+app.on('window-all-closed', () => {
+	if(process.platform !== 'darwin'){
+		app.quit();
+	}
+});
+
+app.on('active', () => {
+	if(app === null){
+		createElectronShell();
+	}
+});
